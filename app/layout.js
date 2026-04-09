1009544851970ddf7c54810a02677dbe68477166
Jeferson Silva <149739943+Jefinhx@users.noreply.github.com>
2026-04-08 23:29:03 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,16 @@
+import React from 'react';
 
+export const metadata = {
+  title: 'Stride 2.0',
+  description: 'Sincronize seus treinos',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
