320ed898e6b47b7bab519ed56e3dd1a4c02cdbd6
Jeferson Silva <149739943+Jefinhx@users.noreply.github.com>
2026-04-12 21:27:20 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,15 +7,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <head>
-        <style>{`
-          body {
-            margin: 0;
-            padding: 0;
-            overflow-x: hidden;
-          }
-        `}</style>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
-      <body>{children}</body>
+      <body 
+        suppressHydrationWarning={true} 
+        style={{ margin: 0, padding: 0, boxSizing: 'border-box' }} // <--- O TIRO DE MISERICÓRDIA ESTÁ AQUI
+      >
+        {children}
+      </body>
     </html>
   )
 }
